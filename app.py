577a438a1fcf8e5b8d6e3ccb9a394be9b64c2dda
Gary Sentosa <gary.sentosa@gmail.com>
2020-05-28 23:04:42 +0900

    Use SSE instead polling for long task_func

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+import json
 import os
 import random
 import time
-from flask import Flask, request, render_template, session, flash, redirect, \
-    url_for, jsonify
-from flask_mail import Mail, Message
+
 from celery import Celery
+from flask import (
+    Flask,
+    Response,
+    request,
+    render_template,
+    session,
+    flash,
+    redirect,
+    url_for,
+    jsonify
+)
+from flask_mail import Mail, Message
 
 
 app = Flask(__name__)
@@ -97,32 +108,41 @@ def longtask():
 
 @app.route('/status/<task_id>')
 def taskstatus(task_id):
-    task = long_task.AsyncResult(task_id)
-    if task.state == 'PENDING':
-        response = {
-            'state': task.state,
-            'current': 0,
-            'total': 1,
-            'status': 'Pending...'
-        }
-    elif task.state != 'FAILURE':
-        response = {
-            'state': task.state,
-            'current': task.info.get('current', 0),
-            'total': task.info.get('total', 1),
-            'status': task.info.get('status', '')
-        }
-        if 'result' in task.info:
-            response['result'] = task.info['result']
-    else:
-        # something went wrong in the background job
-        response = {
-            'state': task.state,
-            'current': 1,
-            'total': 1,
-            'status': str(task.info),  # this is the exception raised
-        }
-    return jsonify(response)
+    def event_stream(task_id):
+        has_stream = True
+        while has_stream:
+            task = long_task.AsyncResult(task_id)
+            if task.state == 'PENDING':
+                response = {
+                    'state': task.state,
+                    'current': 0,
+                    'total': 1,
+                    'status': 'Pending...'
+                }
+            elif task.state != 'FAILURE':
+                response = {
+                    'state': task.state,
+                    'current': task.info.get('current', 0),
+                    'total': task.info.get('total', 1),
+                    'status': task.info.get('status', '')
+                }
+                if 'result' in task.info:
+                    response['result'] = task.info['result']
+                    has_stream = False
+            else:
+                # something went wrong in the background job
+                response = {
+                    'state': task.state,
+                    'current': 1,
+                    'total': 1,
+                    'status': str(task.info),  # this is the exception raised
+                }
+                has_stream = False
+
+            yield 'data: {}\n\n'.format(json.dumps(response))
+            time.sleep(1)
+
+    return Response(event_stream(task_id), mimetype='text/event-stream')
 
 
 if __name__ == '__main__':
